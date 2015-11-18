9349a58d6ad492a342d518406cb7f64b863e869b
Bradley Lockhart <blockhar@ualberta.ca>
2015-11-17 21:09:38 -0700

    some annoying indentation error or something

--- interpreter.py
+++ interpreter.py
@@ -1,16 +1,11 @@
 import re
 import sys
 import fileinput
+#from process import process
+import process
 
 def readQuery(queryStr):
-    #print (queryStr)
     queryParts = splitQuery(queryStr)
-    toProcess = []
-
-    '''while len(queryParts) > 0:
-        term = queryParts[0]
-        if term == ""
-'''
     return (queryParts)
 
 def splitQuery(queryStr):
@@ -92,74 +87,14 @@ def splitQuery(queryStr):
     return finalList
 
 
-def process(queryList):
-	
-	if len(queryList) <= 0:
-		print ("Invalid query: no terms.")
-		sys.exit()
-
-	results = []
-
-	term = queryList.pop(0)
-	
-	if term == 'r:':
-		word = queryList.pop(0)
-		if re.match('\%', word):
-			print("reviewSearch(" + word + ", partial)")
-
-		else:
-			print("reviewSearch(" + word + ")")
-
-	elif re.match('^p:', term):
-        splitTerm = term.split(":", 1)
-        tempName.append("p:")
-        tempName.append(splitTerm[1])
-
-    elif re.match('^rscore<', term):
-        splitTerm = term.split("<", 1)
-        tempName.append("rscore")
-        tempName.append("<")
-        tempName.append(splitTerm[1])
-
-    elif re.match('^rscore>', term):
-        splitTerm = term.split(">", 1)
-        tempName.append("rscore")
-        tempName.append(">")
-        tempName.append(splitTerm[1])
-
-    elif re.match('^pprice<', term):
-        splitTerm = term.split("<", 1)
-        tempName.append("pprice")
-        tempName.append("<")
-        tempName.append(splitTerm[1])
-
-    elif re.match('^pprice>', term):
-        splitTerm = term.split(">", 1)
-        tempName.append("pprice")
-        tempName.append(">")
-        tempName.append(splitTerm[1])
-
-    elif re.match('^rdate<', term):
-        tempName.append("rdate")
-
-    elif re.match('^rdate>', term):
-        tempName.append("rdate")
-
-    elif re.match('^>.+', term):
-        tempName.append(">")
-
-    elif re.match('^<.+', term):
-        tempName.append("<")	
-	
-	return()
-
-
 
 if __name__ == '__main__':
-    for line in fileinput.input():
-    	queryList = readQuery(line.strip())
-	process(queryList)
-
+	for line in fileinput.input():
+		queryList = readQuery(line.strip())
+		process.process(queryList)
+		print()
+		print ("Next query.")
+		print()
 
 
 
