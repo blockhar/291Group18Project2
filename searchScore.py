c384b8db33b0bd5c4f520482b3f2fac851986814
Bradley Lockhart <blockhar@cs.ualberta.ca>
2015-11-21 13:22:34 -0700

    first real working version

--- searchScore.py
+++ searchScore.py
@@ -1,6 +1,10 @@
 from bsddb3 import db
 import sys
 
+def toNum(num):
+	num = num.replace("'","")
+	return(eval(num))
+
 def searchScores(score, operator):
 	index = db.DB()
 	index.open("sc.idx")
@@ -11,14 +15,16 @@ def searchScores(score, operator):
 
 	if operator == '<':
 		iter = curs.first()
-		while iter and iter[1] < score:
-			results.append(iter[1])
+		while iter and toNum(iter[0].decode('utf-8')) < score:
+			results.append(iter[1].decode('utf-8'))
 			iter = curs.next()
 
 	elif operator == '>':
-		iter = curs.set_range(score)
+		iter = curs.set_range(str(score).encode('utf-8'))
+		if toNum(iter[0].decode('utf-8')) == score:
+			iter = curs.next_nodup()
 		while iter:
-			results.append(iter[1])
+			results.append(iter[1].decode('utf-8'))
 			iter = curs.next()
 
 	else:
@@ -26,3 +32,11 @@ def searchScores(score, operator):
 		sys.exit()
 
 	return results
+
+if __name__ == '__main__':
+	score = eval(input("Input score: "))
+	op = input("Operator: ")
+
+	results = searchScores(score,op)
+	
+	print(results)
