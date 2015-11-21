ee99b04a14b03a188a7ca1a16b633d7d6d82e1b6
fdrury <fdrury@ualberta.ca>
2015-11-21 16:11:30 -0700

    added colons

--- dateSearch.py
+++ dateSearch.py
@@ -2,7 +2,7 @@ import sys
 from bsddb3 import db
 
 # theData is type date and isGreater is boolean
-def dateSearch(theDate, isGreater)
+def dateSearch(theDate, isGreater):
     index = db.DB()
     index.open("pt.idx")
     
@@ -12,8 +12,8 @@ def dateSearch(theDate, isGreater)
     dateIndex #initialize this!
     idIndex #initialize this!
     
-    while row = curs.next()
-        if (row[dateIndex] > theDate and isGreater) or (row[dateIndex] < theDate and !isGreater)
+    while row = curs.next():
+        if (row[dateIndex] > theDate and isGreater) or (row[dateIndex] < theDate and !isGreater):
             results.append(row[idIndex])
     
     curs.close()
