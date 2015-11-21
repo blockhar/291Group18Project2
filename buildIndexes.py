a803ed501a5fa7a175d4d87f78d23980470afb71
fdrury <fdrury@ualberta.ca>
2015-11-21 13:28:25 -0700

    weird syntax error

--- buildIndexes.py
+++ buildIndexes.py
@@ -50,10 +50,10 @@ def buildIndexes():
   sc.idx = db.DB()
   
   # import  text files and create indexes
-  db_load -c -T -t hash -f reviews.txt rw.idx
-  db_load -c -T -t btree -f pterms.txt pt.idx
-  db_load -c -T -t btree -f rterms.txt rt.idx
-  db_load -c -T -t btree -f scores.txt sc.idx
+  db_load -c duplicates=1 -T -t hash -f reviews.txt rw.idx
+  db_load -c duplicates=1 -T -t btree -f pterms.txt pt.idx
+  db_load -c duplicates=1 -T -t btree -f rterms.txt rt.idx
+  db_load -c duplicates=1 -T -t btree -f scores.txt sc.idx
   
   # store indexes
   db_dump -f rw.idx
