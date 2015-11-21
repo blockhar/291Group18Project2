c4c69d1eeefebeb23a78bb04800e0aaae6539bf6
fdrury <fdrury@ualberta.ca>
2015-11-21 15:52:33 -0700

    fixed indentation

--- dateSearch.py
+++ dateSearch.py
@@ -3,18 +3,19 @@ from bsddb3 import db
 
 # theData is type date and isGreater is boolean
 def dateSearch(theDate, isGreater)
-    index = db.DB()
+	index = db.DB()
 	index.open("pt.idx")
-    
+	
 	curs = index.cursor()
 	
 	results = []
 	dateIndex #initialize this!
 	idIndex #initialize this!
-    
-    while row = curs.next()
+	
+	while row = curs.next()
         if (row[dateIndex] > theDate and isGreater) or (row[dateIndex] < theDate and !isGreater)
             results.append(row[idIndex])
     
-    curs.close()
-	return results
+    
+    return results
+    
