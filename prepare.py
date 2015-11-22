ee80643ed6f65aaca11454c0369dbd3ae682534f
lexiebartlettt <lexie@ualberta.ca>
2015-11-22 14:08:21 -0700

    FIXED IT FINALLY

--- prepare.py
+++ prepare.py
@@ -10,47 +10,47 @@ def ReplaceChar(line):
 
 def parsefile(line,review_line,review_file,pterm_file,rterm_file,score_file,review_index):
 
-	if 'product/productId' in line: 
+	if 'product/productId: ' in line: 
 		review_index += 1 
 		line = line.replace('product/productId: ','')
 		review_line = str(review_index) + "," + line +","
 
-	elif 'product/title' in line: 
+	elif 'product/title: ' in line: 
 		line = line.replace('product/title: ','')
 		review_line += "\"" +line+ "\","
 		Terms(line,pterm_file,review_index)
 
-	elif 'product/price' in line: 
+	elif 'product/price: ' in line: 
 		line = line.replace('product/price: ','')
 		review_line += line + ","
 		
-	elif 'review/userId' in line: 
+	elif 'review/userId: ' in line: 
 		line = line.replace('review/userId: ','')
 		review_line += line + ","
 	
-	elif 'review/profileName' in line: 
+	elif 'review/profileName: ' in line: 
 		line = line.replace('review/profileName: ','')
 		review_line += "\"" +line+ "\","
 
-	elif 'helpfulness' in line: 
+	elif 'review/helpfulness:' in line: 
 		line = line.replace('review/helpfulness: ','')
 		review_line += line+ ","
 
-	elif 'review/score' in line: 
+	elif 'review/score: ' in line: 
 		line = line.replace('review/score: ','')
 		review_line += line + ","
 		Scores(line,score_file,review_index)
 
-	elif 'review/time' in line: 
+	elif 'review/time: ' in line: 
 		line = line.replace('review/time: ','')
 		review_line += line+ ","
 
-	elif 'review/summary' in line: 
+	elif 'review/summary: ' in line: 
 		line = line.replace('review/summary: ','')
 		review_line += "\"" +line+ "\","
 		Terms(line,rterm_file,review_index)
 
-	elif 'review/text' in line: 
+	elif 'review/text: ' in line: 
 		line = line.replace('review/text: ','')
 		review_line += "\"" +line+ "\""
 		ReviewPrint(review_line, review_file)
@@ -90,7 +90,7 @@ def Scores(score,f, review_index):
 if __name__=="__main__":
 	review_index = 0
 	review_line = ""
-	review_file = open('review.txt','w')
+	review_file = open('reviews.txt','w')
 	pterm_file  = open('pterms.txt','w')
 	rterm_file  = open('rterms.txt','w')
 	score_file  = open('scores.txt', 'w')
