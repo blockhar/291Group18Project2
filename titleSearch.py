ce8e1cfe2ca8bfb044abdc83e5fb1d07b927d93b
Bradley Lockhart <blockhar@cs.ualberta.ca>
2015-11-22 13:19:20 -0700

    Works for full match

--- titleSearch.py
+++ titleSearch.py
@@ -13,17 +13,30 @@ def searchTitles(word, partial):
 	results = []
 
 	if partial:
+		print("Doing partial.")
 		if word[-1] == "%":
 			word = word[:-1]
 		maxWord = word[:-1] + incrementLastLetter(word,1)
-		iter = curs.set_range(word)
-		while iter and iter[1] < maxWord:
-			results.append(iter[1])
+		iter = curs.set_range(word.encode('utf-8'))
+		while iter and iter[1].decode('utf-8') < maxWord:
+			results.append(iter[1].decode('utf-8'))
 			iter = curs.next()
 	else:
-		iter = curs.get(word)
+		print("Doing Full.")
+		#print(word)
+		#print(word.encode('utf-8'))
+		iter = curs.set(word.encode('utf-8'))
 		while iter:
 			results.append(iter[1])
 			iter = curs.next_dup()
 
 	return results
+
+
+if __name__ == '__main__':
+	word = input("Word: ")
+	partial = eval(input("Partial: "))
+
+	results = searchTitles(word, partial)
+
+	print(results)
