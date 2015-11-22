a29aee2c65276eb31c66396481f562ffa641d7ee
fdrury <fdrury@ualberta.ca>
2015-11-21 23:02:54 -0700

    should work

--- titleSummaryReviewSearch.py
+++ titleSummaryReviewSearch.py
@@ -1,4 +1,4 @@
 def titleSummaryReviewSearch(word, partial):
     Uniquely combines results of two other funcitions
-    return searchTitles(word, partial) + list(set(searchReviewSummary(word, partial)) - set(searchTitles(word, partial)))
+    return searchTitles(word, partial) + list(set(searchFilesSummary(word, partial)) - set(searchTitles(word, partial)))
     
