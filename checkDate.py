31d2fb32f6de001ba286e39f9020422603f2548f
fdrury <fdrury@ualberta.ca>
2015-11-23 12:21:34 -0700

    added main

--- checkDate.py
+++ checkDate.py
@@ -1,13 +1,16 @@
 import sys
 import re
-from bsddb3 import db
 
 # information is all of the product information
-def checkDate(information, reviewDate, lowDate, highDate):
+def checkDate(information, lowDate, highDate):
     if lowDate == -1 and highDate == -1:
       return true
     
     #convert lowDate and/or highDate to correct format
+    if not lowDate == -1:
+    	lowDate = datetime.strptime(lowDate, '%Y/%m/%d')
+    if not highDate == -1:
+    	highDate = datetime.strptime(highDate, '%Y/%m/%d')
     
     dateIndex = 7 # this is the minimum possible value
     
@@ -20,6 +23,7 @@ def checkDate(information, reviewDate, lowDate, highDate):
     return false
 
 if __name__ == '__main__':
-	#Might need to convert string to date here
-	#checkDate(1182816000, 1)
-	#checkDate(1182816000, 0)
+	print checkDate('B000GKXY34,Nun Chuck,Novelty Nun Toss Toy,17.99,ADX8VLDUOL7BG,M. Gingras,0/0,5.0,1262304000,Great fun!,Got these last Christmas as a gag gift. They are great fun, but obviously this is not a toy that lasts!','1994/01/01','1995/01/01')
+	print checkDate('B000GKXY34,Nun Chuck,Novelty Nun Toss Toy,17.99,ADX8VLDUOL7BG,M. Gingras,0/0,5.0,1262304000,Great fun!,Got these last Christmas as a gag gift. They are great fun, but obviously this is not a toy that lasts!','1994/01/01','2015/10/10')
+	print checkDate('B000GKXY34,Nun Chuck,Novelty Nun Toss Toy,17.99,ADX8VLDUOL7BG,M. Gingras,0/0,5.0,1262304000,Great fun!,Got these last Christmas as a gag gift. They are great fun, but obviously this is not a toy that lasts!',-1,'2015/10/10')
+	print checkDate('B000GKXY34,Nun Chuck,Novelty Nun Toss Toy,17.99,ADX8VLDUOL7BG,M. Gingras,0/0,5.0,1262304000,Great fun!,Got these last Christmas as a gag gift. They are great fun, but obviously this is not a toy that lasts!','1994/01/01',-1)
