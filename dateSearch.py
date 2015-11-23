5a08e455c1c930d32fcd2ed3d608cdc5b86b8625
fdrury <fdrury@ualberta.ca>
2015-11-23 11:32:33 -0700

    updated

--- dateSearch.py
+++ dateSearch.py
@@ -1,21 +1,24 @@
 import sys
+import re
 from bsddb3 import db
 
 # theData is type date and isGreater is boolean
 def dateSearch(theDate, isGreater):
     index = db.DB()
-    index.open("rw.idx")
+    index.open("rw.idx") #first column: index, second column: everything else
     
     curs = index.cursor()
     
     results = []
-    dateIndex #initialize this!
-    idIndex #initialize this!
+    dateIndex = 7
+    idIndex = 0
     
     while row = curs.next():
-        if (row[dateIndex] > theDate and isGreater) or (row[dateIndex] < theDate and !isGreater):
-            results.append(row[idIndex])
+        rowArray = row[1].split(",")
+        if (rowArray[dateIndex] > theDate and isGreater) or (rowArray[dateIndex] < theDate and !isGreater):
+            results.append(rowArray[idIndex])
     
     curs.close()
+    index.close()
     return results
     
