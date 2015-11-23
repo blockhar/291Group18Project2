7d003033c7c133560ea75bf0d80773711b9191d8
fdrury <fdrury@ualberta.ca>
2015-11-23 12:47:00 -0700

    trying bitwise xor

--- checkDate.py
+++ checkDate.py
@@ -22,7 +22,7 @@ def checkDate(information, lowDate, highDate):
     while not re.match('[0-9]{10}', infoArray[dateIndex]):
       dateIndex += 1
     
-    if not(lowDate != -1 or lowDate < int(infoArray[dateIndex])) and not(highDate != -1 or highDate > int(infoArray[dateIndex])):
+    if not(lowDate != -1 ^ lowDate < int(infoArray[dateIndex])) and not(highDate != -1 ^ highDate > int(infoArray[dateIndex])):
       return True
     return False
 
