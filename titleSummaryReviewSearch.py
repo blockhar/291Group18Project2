270d54a8808e83084b054ce78b2c38a9257b26b8
Bradley Lockhart <blockhar@cs.ualberta.ca>
2015-11-23 11:37:33 -0700

    Removed

--- titleSummaryReviewSearch.py
+++
@@ -1,4 +0,0 @@
-def titleSummaryReviewSearch(word, partial):
-    #Uniquely combines results of two other functions
-    return searchTitles(word, partial) + list(set(searchFilesSummary(word, partial)) - set(searchTitles(word, partial)))
-    
